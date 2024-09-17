a2d6de80a595a5271f83ee8dbd7c02cf05e75e13
OKaikson <56567769+OKaikson@users.noreply.github.com>
2024-09-17 15:56:09 -0400

    155613092024

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -2,6 +2,8 @@ import styled, {keyframes, css} from "styled-components";
 
 export const Container = styled.div`
     max-width: 700px;
+    min-width: 350px;
+    width: 90%;
     background: #fff;
     border-radius: 4px;
     box-shadow: 0 0 20px rgba(0,0,0, 0.2);
